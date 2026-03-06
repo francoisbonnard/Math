4aa0efd088c4d3401b5ae48355ae26244aa55fc5
francois bonnard <francois.bonnard@gmail.com>
2026-03-06 19:59:47 +0100

    snowflake

--- factals/SnowFlake.py
+++ factals/SnowFlake.py
@@ -2,18 +2,28 @@ from turtle import  *
 # shape("tree")
 speed(0.5)
 
-def snowflake(size, levels):
-    if levels == 0:
-        forward(size)
-        return
-    snowflake(size/3, levels-1)
-    left(60)
-    snowflake(size/3, levels-1)
-    right(120)
-    snowflake(size/3, levels-1)
-    left(60)
-    snowflake(size/3, levels-1)     
+def snowflake_side(length, levels):
+	if levels == 0:
+		forward(length)
+		return
+
+	length /= 3.0
+	snowflake_side(length, levels - 1)
+	left(60)
+	snowflake_side(length, levels - 1)
+	right(120)
+	snowflake_side(length, levels - 1)
+	left(60)
+	snowflake_side(length, levels - 1)
+
+
+def create_snowflake(sides, length):
+	colors = ["green", "blue", "yellow", "orange"]
+	for i in range(sides):
+		color(colors[i])
+		snowflake_side(length, sides)
+		right(360 / sides)
     
 
-snowflake(200, 4)
+create_snowflake(4, 200)
 mainloop()
